0e75bba0bb9463b6754cdcde641506da2561cfd5
ivan2899 <98617252+ivan2899@users.noreply.github.com>
2023-10-29 20:59:13 -0300

    primer commit

---
+++ js/pasajePal.js
@@ -0,0 +1,11 @@
+
+
+function ConvertirTexto(idTexto, idOpcion){
+    let texto = document.getElementById(idTexto).value;
+    /*let auxOpcion = document.getElementById(idOpcion).value;
+    let opcion = parseInt(auxOpcion);*/
+
+    alert("a")
+    alert(opcion + " a " + texto)
+
+}
